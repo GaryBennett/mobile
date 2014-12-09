83ba594936d93abc237550f27b73281a36086f09
alvar <mahoihei@gmail.com>
2014-12-09 09:57:21 +0000

    f

--- app/src/main/java/edu/mobile/assignment/data/DBHelper.java
+++ app/src/main/java/edu/mobile/assignment/data/DBHelper.java
@@ -1,8 +1,14 @@
 package edu.mobile.assignment.data;
 
 import android.content.Context;
+import android.content.res.AssetManager;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * Created by alvar on 26/11/14.
@@ -10,16 +16,20 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class DBHelper extends SQLiteOpenHelper {
 
     public static final String DB_NAME = "lecture";
+    private Context context;
 
     public DBHelper(Context context, SQLiteDatabase.CursorFactory factory) {
-        super(context, DB_NAME, factory, 8);
+        super(context, DB_NAME, factory, 14);
+        this.context = context;
     }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
 
-        sqLiteDatabase.execSQL(LectureDataModel.LectureEntity.CREATE_SQL);
-        sqLiteDatabase.execSQL(LectureDataModel.LectureEntity.INSERT_DATA);
+        execSqlFile(sqLiteDatabase,readSqlFile());
+
+//        sqLiteDatabase.execSQL(LectureDataModel.LectureEntity.CREATE_SQL);
+//        sqLiteDatabase.execSQL(LectureDataModel.LectureEntity.INSERT_DATA);
 
     }
 
@@ -27,8 +37,30 @@ public class DBHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i2) {
 
         sqLiteDatabase.execSQL(LectureDataModel.LectureEntity.DROP_SQL);
-        sqLiteDatabase.execSQL(LectureDataModel.LectureEntity.CREATE_SQL);
-        sqLiteDatabase.execSQL(LectureDataModel.LectureEntity.INSERT_DATA);
+        execSqlFile(sqLiteDatabase,readSqlFile());
+//        sqLiteDatabase.execSQL(LectureDataModel.LectureEntity.CREATE_SQL);
+//        sqLiteDatabase.execSQL(LectureDataModel.LectureEntity.INSERT_DATA);
 
+    }
+
+    private String readSqlFile(){
+        AssetManager assetManager = context.getAssets();
+        StringBuilder sql = new StringBuilder();
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(assetManager.open("db.sql")));
+            String line;
+            while ((line = reader.readLine()) != null){
+                sql.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return sql.toString();
+    }
+
+    private void execSqlFile(SQLiteDatabase sqLiteDatabase,String sql){
+        for(String sqlInstruction : sql.split(";")){
+            sqLiteDatabase.execSQL(sqlInstruction);
+        }
     }
 }
